0edf9b34654abd00aa5d7cd8a148d0909b3153e7
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-06 23:53:47 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 01/D. Difference.cpp
@@ -0,0 +1,13 @@
+//D. Difference
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   long long X,A,B,C,D;
+   cin>>A>>B>>C>>D;
+   cout<< "Difference = "<<(A*B)-(C*D)<<endl;
+    return 0;
+}
