f10b7feb30580aab2539f9614688c0866022b2ef
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-06 23:55:29 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 01/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,24 @@
+//  I. Welcome for you with Conditions
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  int a ,b ;
+  cin>>a;
+  cin>>b ;
+  if (a>=b)
+  {
+    cout<< "Yes"<<endl;
+  }
+  else 
+  {
+     cout<< "No"<<endl;
+
+  }
+
+    return 0;
+}
