5adfe3c7540c049781f0c598fe79fe2834c6f1be
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-07 18:16:38 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 01/L. The Brothers.cpp
@@ -0,0 +1,26 @@
+// L. The Brothers
+
+#include <iostream>
+#include <cmath>
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main()
+{
+
+string str1="", str2="";
+string str3="", str4="";
+cin>>str1>>str2>>str3>>str4;
+
+if(str2 ==str4)
+{
+   cout<< "ARE Brothers"<<endl;
+}
+else 
+{
+    cout<<"NOT"<<endl;
+}
+
+return 0;
+}
