cb30ef177b548f019efc984a5707b05954e98df0
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-07 18:18:34 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 01/P. First digit !.cpp
@@ -0,0 +1,30 @@
+//  P. First digit !
+# include <iostream>
+using namespace std ;
+int main ()
+{
+
+
+  int num ;   // (999 < X  ≤  9999)   ,  any number = 4 digit ===>(????) 
+  cin >>num;
+ int result =num /1000 ;   //  ???? /1000 = ?.???   
+
+if (result %2 == 0)
+{
+  cout<<"EVEN"<<endl;
+}
+else
+{
+    cout<<"ODD"<<endl;
+
+}
+ 
+
+return 0;
+}
+
+
+
+
+
+  
