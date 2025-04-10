b2891047496589867db0cddd33eb7ff981192cdb
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-10 23:40:00 +0200

    Assuit Sheet Solution By C++

---
+++ Contest 01/F. Adding Bits.cpp
@@ -0,0 +1,19 @@
+// F. Adding Bits
+
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+
+int a, b;
+cin >>a>>b;
+ int c=a^ b;  // ^    ====> XOR 
+cout<< c<<endl;
+
+
+
+ return 0;
+}
+
+
