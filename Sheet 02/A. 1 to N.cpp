13289012241c104343f8ddd22a336fa3c9265260
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-12 23:50:58 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 02/A. 1 to N.cpp
@@ -0,0 +1,21 @@
+// A. 1 to N
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    int counter;
+    cin>>counter;
+    for( int i=1 ; i<=counter ; i++)
+    {
+        cout<<i<<endl;
+    }
+
+
+return 0;
+}
+
+
+
+
+
