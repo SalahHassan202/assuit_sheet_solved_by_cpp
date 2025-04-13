c883a4ca813fbd20aeb45168c2f400897cba614f
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-13 23:46:45 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 02/F. Multiplication table.cpp
@@ -0,0 +1,20 @@
+// F. Multiplication table
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+// int counter =12;
+int num;
+cin>>num ;
+for (int i=1 ;i<=12 ;i++)
+{
+   cout<< num<<" * "<<i<<" = "<<num*i<<endl;
+}
+return 0;
+}
+
+
+
+
+
