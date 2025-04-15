cc8af2dcf2cb59eb83d7eab53d2d7cf8a154ca37
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-15 18:40:17 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 02/N. Numbers Histogram.cpp
@@ -0,0 +1,35 @@
+// N. Numbers Histogram
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+
+char s ;   //symbol
+int lines;
+
+cin>>s>>lines ;
+
+for (int i=1 ;i<=lines ;i++)
+{
+    int num ;  //times 
+    cin>>num ;
+for(int j=1 ;j<=num ;j++)
+{
+    cout<<s;
+}
+cout <<endl;
+}
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
