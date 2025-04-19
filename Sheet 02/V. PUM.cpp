0731eb699e6cdcdf6ce4ef86aeb52a827d693dd3
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-19 22:02:47 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 02/V. PUM.cpp
@@ -0,0 +1,38 @@
+// V. PUM
+# include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+
+int lines;
+cin>>lines;
+int counter =1;
+
+for (int i=1 ; i<=lines ;i++)
+{
+    if(counter % 4 == 0)
+    {
+        counter++;
+        cout<<"PUM"<<endl;
+        i--;
+        
+        continue ;
+    }
+cout<<counter++<<" "<<counter++<<" "<<counter++<<" ";
+
+}
+
+  cout<<"PUM"<<endl;
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
