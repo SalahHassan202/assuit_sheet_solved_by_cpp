bf4022aede63b46ca1621c51e4215237f5cf2b7d
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-25 22:56:31 +0200

    Assuit Sheet Solution By C++

---
+++ Contest 02/H. Simple Mod.cpp
@@ -0,0 +1,16 @@
+// H. Simple Mod
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int num ;
+cin>>num ;
+
+cout<< "0 0"<<endl;
+//    (X^2+Y^2)% num ==0   =====> every x and y =0 the result is equal 0    because ====> 0 % num = 0 
+
+    return 0;
+}
+
+
