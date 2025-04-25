029e62faaea44f37c5449f59c37519a40f7dd68d
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-25 22:53:44 +0200

    Assuit Sheet Solution By C++

---
+++ Contest 02/A. Timon and Pumbaa.cpp
@@ -0,0 +1,23 @@
+// A. Timon and Pumbaa
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int a ,b ;
+cin>> a >> b ;
+
+if (a-b <=0)
+{
+    cout<<0<<endl;
+}
+else
+{
+    cout <<abs(a-b)<<endl;
+}
+
+
+
+
+    return 0;
+}
