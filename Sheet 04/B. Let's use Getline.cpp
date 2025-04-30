b93eccd345418d4018cf75299a16f03067468bb7
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-04-30 22:54:22 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 04/B. Let's use Getline.cpp
@@ -0,0 +1,22 @@
+// B. Let's use Getline
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+   string s1;
+   getline(cin,s1);
+   for(int i =0 ; i<s1.size() ; i++)
+   {
+      if (s1[i] == '\\')
+      {
+         break;
+      }
+         cout<<s1[i];
+   }
+
+
+   return 0 ;
+}
