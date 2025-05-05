b95d37bf2cd7db1c5f0f5614ace89aa3502600fa
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-05 22:56:01 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 04/Z. Clean Code.cpp
@@ -0,0 +1,48 @@
+// Z. Clean Code
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string line;
+	int open =1;
+	
+	while(getline(cin , line ))
+	{
+		bool flag = 0;
+		if(line == " " || line.size() == 0)
+		{
+			continue;    // ignore this line 
+		}
+		for(int i =0 ; i<line.length() ; i++)
+		{
+			if(line[i] == '/'  && line[i+1]== '/' && open )
+			{
+				break;
+			}
+			else if (line[i] == '/'  && line[i+1]== '*')
+			{
+				i++;
+				open =0;
+			}
+			else if(line[i] == '*'  && line[i+1]== '/' && !open )
+			{
+				i++;
+				open=1;
+			}
+			else if(open ==1 )
+			{
+				cout<<line[i];
+				flag =1 ;
+			}
+		}
+		if(flag && open ==1)
+		{
+			cout<<endl;
+		}
+	}
+	
+
+	return 0;
+	
+}
