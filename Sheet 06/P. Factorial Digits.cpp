93f3608ee4f83caa465e145ec937dca896bb555a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-16 23:09:59 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 06/P. Factorial Digits.cpp
@@ -0,0 +1,20 @@
+// P. Factorial Digits
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+	double number , result;
+	cin>>number;
+	for(int i =2 ; i<=number ; i++)
+	{
+		 result+= log10(i) ;
+
+	}
+	cout<<"Number of digits of "<< number<< "! is "<< (int)result+1 <<endl;
+
+	return 0;
+	
+}
