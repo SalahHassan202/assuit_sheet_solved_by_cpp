2922a0fd0ba54f5af625fd0ce95d00eba732dbe5
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-05-23 23:00:09 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 08/B. Watermelon.cpp
@@ -0,0 +1,20 @@
+// B. Watermelon
+#include <iostream>
+using namespace std;
+int main()
+{
+
+int w ;        // Watermelon
+cin>>w ;
+
+if(w % 2 == 0 && w > 2)
+{
+    cout<< "YES"<<endl;
+}
+else
+{
+    cout<< "NO"<<endl;
+}
+
+return 0;
+}
