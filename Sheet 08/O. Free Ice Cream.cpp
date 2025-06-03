b754fc8292b7a616e934ce826d734bd39f369027
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-06-03 15:46:45 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 08/O. Free Ice Cream.cpp
@@ -0,0 +1,38 @@
+// O. Free Ice Cream
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std; 
+int main()
+{
+	long long cases , iceCream ;
+	cin >> cases >> iceCream ;
+
+	long long counter = 0 ;
+
+	for (int i =0 ; i < cases ; i++)
+	{
+		char oper;      long long num ;
+		cin >> oper >> num ;
+
+		if( oper == '+')
+		{
+			iceCream+= num;            // iceCream = iceCream + num ;
+		}
+		else       //  (oper == '-')
+		{
+			if( iceCream >= num)
+			{
+				iceCream-=num ;       // iceCream = iceCream - num ;
+			} else
+			{
+				counter++;
+			}
+		}
+	}
+	
+	cout<< iceCream <<" "<< counter << endl;
+
+
+	return 0;
+}
