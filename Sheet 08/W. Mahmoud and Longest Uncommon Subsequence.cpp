0cfca302dca4d5778ec43c929d8c44e91183f532
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-06-03 15:48:15 +0200

    Assuit Sheet Solution By C++

---
+++ Sheet 08/W. Mahmoud and Longest Uncommon Subsequence.cpp
@@ -0,0 +1,44 @@
+// W. Mahmoud and Longest Uncommon Subsequence
+#include <iostream>
+#include <string>
+using namespace std;
+
+int find_uncommon_subsequence_length( string a , string b)
+{
+    if (a == b)
+    {
+        return -1; // No uncommon subsequence
+    }
+
+    return max(a.length(), b.length());
+}
+
+int main()
+{
+
+    /*
+
+	abc 
+	sub sequence is 
+	a
+	b
+	c
+	ab
+	ac
+	bc
+	abc
+	---------
+	7 sub suequence 
+	.........etc 
+	
+	*/
+
+
+    string a, b;
+    cin >> a >> b;
+
+    int result = find_uncommon_subsequence_length(a, b);
+    cout << result << endl;
+
+    return 0;
+}
