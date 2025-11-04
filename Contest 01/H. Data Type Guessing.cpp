1661cd525f6c0aa21c4d7c1a8993a55bb9aa69e4
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-04 12:37:10 +0200

    Update H. Data Type Guessing.cpp

--- Contest 01/H. Data Type Guessing.cpp
+++ Contest 01/H. Data Type Guessing.cpp
@@ -1,39 +1,53 @@
-// H. Data Type Guessing
+//  H. Data Type Guessing
 
-# include <iostream>
 #include <bits/stdc++.h>
-using namespace std ;
-int main ()
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
 {
 
 double n,k,a,s;
 cin>>n>>k>>a;
 
 s=(n*k)/a ;         //68.7
-long long y =s ;    // 68
+ll y =s ;           // 68
 double z =s-y ;     //.7
 
 
 if (z>0 )
 {
     cout<<"double"<<endl;
-    return 0;
+    return ;
 }else 
 {
     if (y<=2147483647)
-    {
-        cout<<"int"<<endl;
-    }
+    cout<<"int"<<endl;
     else 
+    cout<<"long long"<<endl;
+    
+}
+
+
+}
+
+int main()
+{
+    fast;
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
     {
-        cout<<"long long"<<endl;
+        solve();
     }
+
+    return 0;
 }
 
-
-
-
- return 0;
-}
 
 
