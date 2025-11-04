6cad608da8b447c2784c3ff6f21e4ecee783b4db
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-04 12:39:48 +0200

    Update I. Lucky Numbers.cpp

--- Contest 01/I. Lucky Numbers.cpp
+++ Contest 01/I. Lucky Numbers.cpp
@@ -1,34 +1,48 @@
-// I. Lucky Numbers
-# include <iostream>
-#include <bits/stdc++.h>
-using namespace std ;
-int main ()
-{
+//  I. Lucky Numbers
 
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
 
 string a ;    // "65"     ['6' , '5' ]
 cin>>a;    
-int num1 =a[0] -'0';
-int num2 =a[1] -'0';
+ll n1 =a[0] -'0';
+ll n2 =a[1] -'0';
 //   cout <<a[0]<< "   "<<a[1]<<endl;    ====>    6    5
-if(num2 ==0)
+if(n2 ==0)
 {
     cout<<"YES"<<endl;
-    return 0;
-}   
-if (num1% num2 ==0 || num2 %num1 ==0)
-{
-    cout<<"YES"<<endl;
-}
+    return ;
+} 
+
+if (n1% n2 ==0 || n2 %n1 ==0)
+cout<<"YES"<<endl;
 else
+cout<<"NO"<<endl;
+
+
+}
+
+int main()
 {
-    cout<<"NO"<<endl;
+    fast;
+
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
 }
-
-return 0;
-}
-
-
 
 
 
