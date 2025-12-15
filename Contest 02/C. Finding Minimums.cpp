8d752f733f6fa8b8937219154b75d5067265e774
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-15 22:35:31 +0200

    Update C. Finding Minimums.cpp

--- Contest 02/C. Finding Minimums.cpp
+++ Contest 02/C. Finding Minimums.cpp
@@ -1,44 +1,57 @@
 // C. Finding Minimums
-#include <iostream>
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 using namespace std;
+
+void solve()
+{
+    int size , sequance;
+    cin>> size >>sequance;
+    int counter =0 ;
+    int min , flag =1 ;
+
+    for(int i= 1 ; i<=size ;i++)
+    {
+        int number;
+        cin>>number;
+        if(flag)
+        {
+            min =number;
+            flag=0;
+        }
+        else
+        {
+            if(number< min)
+            {
+                min=number;
+            }
+        }
+        counter++;
+
+        if(counter ==sequance || i ==size)
+        {
+            cout<<min<<" ";
+            counter =0 ;
+            flag =1;
+        }
+
+    }
+
+}
+
 int main()
 {
-
-int size , sequance;
-cin>> size >>sequance;
-int counter =0 ;
-int min , flag =1 ;
-
-for(int i= 1 ; i<=size ;i++)
-{
-    int number;
-    cin>>number;
-    if(flag)
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
     {
-        min =number;
-        flag=0;
+        solve();
     }
-    else
-    {
-        if(number< min)
-        {
-            min=number;
-        }
-    }
-    counter++;
-
-    if(counter ==sequance || i ==size)
-    {
-        cout<<min<<" ";
-        counter =0 ;
-        flag =1;
-    }
-
-}
-
-
-
     return 0;
 }
-
-
