d6163eb196e8691f1024f47a38ee7b53d612ebb3
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-15 22:50:45 +0200

    Update A. Square or rectangle.cpp

--- Contest 03/A. Square or rectangle.cpp
+++ Contest 03/A. Square or rectangle.cpp
@@ -1,12 +1,19 @@
 // A. Square or rectangle
-#include <iostream>
-using namespace std;
-int main()
-{
-    int cases ;
-    cin>>cases;
 
-    while(cases--)
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+   int t ;
+    cin>>t;
+
+    while(t--)
     {
         int num1 , num2 ;
         cin>>num1>>num2 ;
@@ -21,8 +28,18 @@ int main()
 
         }
     }
+}
 
-    return 0 ;
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
 
 
