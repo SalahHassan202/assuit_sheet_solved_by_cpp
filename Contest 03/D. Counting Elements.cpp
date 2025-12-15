44b9aefec25a18adc635d617de0ae3a15f7cb148
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-15 22:54:14 +0200

    Update D. Counting Elements.cpp

--- Contest 03/D. Counting Elements.cpp
+++ Contest 03/D. Counting Elements.cpp
@@ -1,8 +1,14 @@
 // D. Counting Elements
-#include <iostream>
-#include<algorithm>
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 using namespace std;
-int main()
+
+void solve()
 {
     int size , temp , result=0 , counter =0;
     cin>>size;
@@ -43,8 +49,18 @@ int main()
         }
     }
     cout<< counter <<endl;
+}
 
-    return 0 ;
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
 
 
