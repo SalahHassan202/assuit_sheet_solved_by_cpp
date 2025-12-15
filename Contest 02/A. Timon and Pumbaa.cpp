57a903bd80aa9d165f28a7afa68c16c4f232fbf9
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-15 22:32:09 +0200

    Update A. Timon and Pumbaa.cpp

--- Contest 02/A. Timon and Pumbaa.cpp
+++ Contest 02/A. Timon and Pumbaa.cpp
@@ -1,23 +1,31 @@
 // A. Timon and Pumbaa
-#include <iostream>
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 using namespace std;
+
+void solve()
+{
+    int a ,b ;
+    cin>> a >> b ;
+
+    if ((a-b) <=0) cout<<0<<endl;
+    else  cout <<abs(a-b)<<endl;
+
+}
+
 int main()
 {
-
-int a ,b ;
-cin>> a >> b ;
-
-if (a-b <=0)
-{
-    cout<<0<<endl;
-}
-else
-{
-    cout <<abs(a-b)<<endl;
-}
-
-
-
-
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
     return 0;
 }
