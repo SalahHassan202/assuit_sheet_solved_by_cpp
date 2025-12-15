b17995b6d53df725ba0e3f839f92d65304a2911a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-15 22:36:48 +0200

    Update D. Range Sum.cpp

--- Contest 02/D. Range Sum.cpp
+++ Contest 02/D. Range Sum.cpp
@@ -1,15 +1,22 @@
 // D. Range Sum
-#include <iostream>
-using namespace std;
-int main()
-{
 
-long long lines ;
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    
+ll lines ;
 cin>>lines;
 
 while(lines--)
 {
-    long long num1 , num2 ,mn,mx;   // mn ===>min    && mx====> max
+    ll num1 , num2 ,mn,mx;   // mn ===>min    && mx====> max
     
     cin>>num1>>num2;
     mx =max(num1 ,num2);
@@ -19,15 +26,24 @@ while(lines--)
 
     mn--;     
 
-    long long result1 = mn*(mn+1)/2  ;    // summition of 0 to num1--
-    long long result2 = mx*(mx+1)/2  ;    // summition of 0 to num2
+    ll result1 = mn*(mn+1)/2  ;    // summition of 0 to num1--
+    ll result2 = mx*(mx+1)/2  ;    // summition of 0 to num2
 
     cout<< result2 -result1 <<endl;
 
 
 }
 
-    return 0;
 }
 
-
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
