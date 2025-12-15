4cf0e6c1bc8a6114f4a022a44569c547eedb8e53
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-15 22:43:00 +0200

    Update H. Simple Mod.cpp

--- Contest 02/H. Simple Mod.cpp
+++ Contest 02/H. Simple Mod.cpp
@@ -1,16 +1,31 @@
 // H. Simple Mod
-#include <iostream>
-using namespace std;
-int main()
-{
 
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
 int num ;
 cin>>num ;
 
 cout<< "0 0"<<endl;
 //    (X^2+Y^2)% num ==0   =====> every x and y =0 the result is equal 0    because ====> 0 % num = 0 
-
-    return 0;
+	
 }
 
-
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
