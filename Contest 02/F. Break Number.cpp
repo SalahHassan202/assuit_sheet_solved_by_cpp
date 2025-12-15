b15866ef105c13a2c4d68d64281541feb45b4a3b
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-15 22:40:26 +0200

    Update F. Break Number.cpp

--- Contest 02/F. Break Number.cpp
+++ Contest 02/F. Break Number.cpp
@@ -1,49 +1,64 @@
 // F. Break Number
-#include <iostream>
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 using namespace std;
-int main()
-{
 
-long long cases , result=0  ;
-cin>>cases;
-for(int i=1 ; i<=cases ;i++)
+void solve()
 {
-    long long value ;
-    cin>>value;
+    ll cases , result=0  ;
+    cin>>cases;
+    for(int i=1 ; i<=cases ;i++)
+    {
+        ll value ;
+        cin>>value;
 
-    int counter=0 ;
-    
-    if(value % 2 !=0)
-    {
-        counter=0;
-    }
-    else
-    {
-        while(value >0)
+        int counter=0 ;
+        
+        if(value % 2 !=0)
         {
-            if(value %2 !=0)
+            counter=0;
+        }
+        else
+        {
+            while(value >0)
             {
-                break;
-            }
-            else
-            {
-               counter++;
-               value/=2;
+                if(value %2 !=0)
+                {
+                    break;
+                }
+                else
+                {
+                counter++;
+                value/=2;
+                }
             }
         }
+        if( counter > result)
+        {
+            result = counter;
+        }
+        
     }
-    if( counter > result)
-    {
-        result = counter;
-    }
-    
+
+        
+    cout<<result<<endl;
+
 }
 
-    
-cout<<result<<endl;
-
-
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
     return 0;
 }
-
 
